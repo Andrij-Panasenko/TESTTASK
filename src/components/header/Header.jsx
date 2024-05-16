832b6eeb132245232855c0fea581f385f6f9b48b
a.panasenko <andrii.panasenko295@gmail.com>
2024-05-16 21:27:34 +0300

    fix wrapping tags in header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import sprite from '../../assets/sprite.svg';
 import '../../css/header/header-mobile.scss';
-import '../../css/_variables.scss'
+import '../../css/_variables.scss';
 import { PrimaryButton } from '../Buttons/PrimaryButton.jsx';
 
 export const Header = () => {
     return (
         <>
-            <div className="container">
-                <header className="header">
-                    <a href="/">
-                        <svg width="104" height="26">
-                            <use xlinkHref={`${sprite}#icon-Logo`}></use>
-                        </svg>
-                    </a>
-                    <ul className="btn-list">
-                        <li>
-                            <PrimaryButton type="button" name="Users" />
-                        </li>
-                        <li>
-                            <PrimaryButton type="button" name="Sign up" />
-                        </li>
-                    </ul>
-                </header>
-            </div>
+            <header>
+                <div className="container">
+                    <div className="header-wrapper">
+                        <a href="/">
+                            <svg width="104" height="26">
+                                <use xlinkHref={`${sprite}#icon-Logo`}></use>
+                            </svg>
+                        </a>
+                        <ul className="btn-list">
+                            <li>
+                                <PrimaryButton type="button" name="Users" />
+                            </li>
+                            <li>
+                                <PrimaryButton type="button" name="Sign up" />
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </header>
         </>
     );
 };
