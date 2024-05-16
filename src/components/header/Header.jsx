07b0575000fa59ea8366bee8213b177500513312
a.panasenko <andrii.panasenko295@gmail.com>
2024-05-16 20:38:17 +0300

    styling header mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,28 @@
-import React from "react";
+import React from 'react';
 import sprite from '../../assets/sprite.svg';
+import '../../css/header.scss';
+import { PrimaryButton } from '../buttons/PrimaryButton.jsx';
 
 export const Header = () => {
     return (
         <>
-            <header>
-                <div>
-                    <svg width="104" height="26">
-                        <use xlinkHref={`${sprite}#icon-Logo`}></use>
-                    </svg>
-                </div>
-                <ul>
-                    <li>
-                        <button type="button">Users</button>
-                    </li>
-                    <li>
-                        <button type="button">Sign up</button>
-                    </li>
-                </ul>
-            </header>
+            <div className="container">
+                <header className="header">
+                    <a href="/">
+                        <svg width="104" height="26">
+                            <use xlinkHref={`${sprite}#icon-Logo`}></use>
+                        </svg>
+                    </a>
+                    <ul className="btn-list">
+                        <li>
+                            <PrimaryButton type="button" name="Users" />
+                        </li>
+                        <li>
+                            <PrimaryButton type="button" name="Sign up" />
+                        </li>
+                    </ul>
+                </header>
+            </div>
         </>
     );
 };
