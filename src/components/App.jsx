914cc939e96d43fcdc4276b970e2ac90235cfe55
a.panasenko <andrii.panasenko295@gmail.com>
2024-05-16 22:28:41 +0300

    styling hero mobile (without bg)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from './header/Header.jsx';
+import { Hero } from './Hero/Hero.jsx';
 
 export default function App() {
     return (
         <>
             <Header />
+            <Hero/>
         </>
     );
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,30 @@
+import React from 'react';
+import { SectionTitle } from '../SectionTitle/SectionTitle.jsx';
+import { PrimaryButton } from '../Buttons/PrimaryButton.jsx';
+import '../../css/_variables.scss';
+import '../../css/hero/hero-mobile.scss';
+
 export const Hero = () => {
     return (
-        <section>
-            
+        <section className="hero-section hero-bg">
+            <div className="container">
+                <SectionTitle
+                    className="hero-title"
+                    title="Test assignment for front-end developer"
+                    tag="h1"
+                />
+                <p className="hero-descr">
+                    What defines a good front-end developer is one that has
+                    skilled knowledge of HTML, CSS, JS with a vast understanding
+                    of User design thinking as they'll be building web
+                    interfaces with accessibility in mind. They should also be
+                    excited to learn, as the world of Front-End Development
+                    keeps evolving
+                </p>
+                <div className="hero-button-wrapp">
+                    <PrimaryButton type="button" name="Sign up" />
+                </div>
+            </div>
         </section>
-    )
-}
+    );
+};
