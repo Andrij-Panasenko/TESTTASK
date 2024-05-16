d79f580da4f63ba189cfea771314f20c28e15363
a.panasenko <andrii.panasenko295@gmail.com>
2024-05-16 21:34:31 +0300

    create SectionTitle component

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,3 +1,11 @@
-export const SectionTitle = ({title}) => {
-    
-}
+import React from 'react';
+
+export const SectionTitle = ({ title, tag, className }) => {
+    const TagName = tag || 'h2';
+
+    return (
+        <>
+            <TagName className={className}>{title}</TagName>
+        </>
+    );
+};
