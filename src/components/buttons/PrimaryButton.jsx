fb0bcefb55190aaa7342b1a724ab1bd0000e02d8
a.panasenko <andrii.panasenko295@gmail.com>
2024-05-17 20:23:58 +0300

    add prop className

--- src/components/buttons/PrimaryButton.jsx
+++ src/components/buttons/PrimaryButton.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import '../../css/primaryButton.scss';
 
-export const PrimaryButton = ({ name, type }) => {
+export const PrimaryButton = ({className, name, type }) => {
+    const buttonClassName = className ? className : 'button';
     return (
-        <button className="button" type={type}>
+        <button className={buttonClassName} type={type}>
             {name}
         </button>
     );
