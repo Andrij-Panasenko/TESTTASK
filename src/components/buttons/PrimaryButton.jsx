ff1bf2dd5b5165693d556f4fcfc993e99ca38bd0
a.panasenko <andrii.panasenko295@gmail.com>
2024-05-17 21:58:43 +0300

    add onCLick prop to PrimaryButton component

--- src/components/buttons/PrimaryButton.jsx
+++ src/components/buttons/PrimaryButton.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../../css/primaryButton.scss';
 
-export const PrimaryButton = ({className, name, type }) => {
+export const PrimaryButton = ({ className, name, type, onClick }) => {
     const buttonClassName = className ? className : 'button';
     return (
-        <button className={buttonClassName} type={type}>
+        <button className={buttonClassName} type={type} onClick={onClick}>
             {name}
         </button>
     );
