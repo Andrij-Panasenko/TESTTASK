a4da5a66191ab1ee7e0a8b23800e12e8be124adb
a.panasenko <andrii.panasenko295@gmail.com>
2024-05-17 22:12:39 +0300

    add params to getUsers.js

--- src/api/getUsers.js
+++ src/api/getUsers.js
@@ -3,10 +3,10 @@ import axios from "axios"
 axios.defaults.baseURL =
     'https://frontend-test-assignment-api.abz.agency/api/v1';
 
-export const getUsers = async () => {
+export const getUsers = async (page, count) => {
     const ENDPOINT = '/users'
     try {
-        const response = await axios.get(`${ENDPOINT}`)
+        const response = await axios.get(`${ENDPOINT}?page=${page}&count=${count}`)
         return response.data;
     } catch (error) {
         console.error('error getting users', error)
