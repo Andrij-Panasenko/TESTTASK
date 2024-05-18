a28062d8538fc73786f80563cd16e1d74f52ed58
a.panasenko <andrii.panasenko295@gmail.com>
2024-05-18 22:22:05 +0300

    add defaul values

--- src/api/getUsers.js
+++ src/api/getUsers.js
@@ -3,7 +3,7 @@ import axios from "axios"
 axios.defaults.baseURL =
     'https://frontend-test-assignment-api.abz.agency/api/v1';
 
-export const getUsers = async (page, count) => {
+export const getUsers = async (page = 1, count = 6) => {
     const ENDPOINT = '/users'
     try {
         const response = await axios.get(`${ENDPOINT}?page=${page}&count=${count}`)
